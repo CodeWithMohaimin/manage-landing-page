e95866d8614f92b85d6b4c24e0867309f16185f6
programmermohaimin <dev.mohaimin@gmail.com>
2022-11-05 18:44:56 +0600

    completed

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ cross.addEventListener('click', function () {
     btn.style.display = 'block';
     cross.style.display ='none';
 })
+
+
+//slider 
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+    loop:true,
+  }); 
